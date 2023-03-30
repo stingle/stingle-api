ba7d28816a5778a65975acfa58781c5467194c7b
Alex Amiryan <git@fenritz.com>
2023-03-30 05:06:37 +0400

    Fix in setup for debian systems

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ if ! [ -x "$(command -v docker)" ]; then
       mkdir -p /etc/apt/keyrings
       curl -fsSL https://download.docker.com/linux/$ID/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
       echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$ID \
         $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
       apt-get update
       apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
